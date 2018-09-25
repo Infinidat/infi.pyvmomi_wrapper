3c7f11c88bdf08cff33819bf7dcc4bc65958ddc2
Arnon Yaari <arnony@infinidat.com>
2018-09-25 11:39:18 +0300

    Revert "Workaround for issue vmware/pyvmomi#514"
    This reverts:
    commit ce257d26c54cbb2831a03c4ae1edad34b62fb90e and
    commit 6c1a5c11f580e19518e87639b7777c06550d6cbc

--- src/infi/pyvmomi_wrapper/__init__.py
+++ src/infi/pyvmomi_wrapper/__init__.py
@@ -2,4 +2,3 @@ __import__("pkg_resources").declare_namespace(__name__)
 from .client import Client, get_reference_to_managed_object
 from .tasks import TaskManager, Task
 from .property_collector import CachedPropertyCollector
-from . import MoreObjects
